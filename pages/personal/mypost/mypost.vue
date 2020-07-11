7d8a834150b549d22c410a5260968156fd30fe86
liuzhihui <1341746911@qq.com>
2020-07-11 16:45:02 +0800

    暂存

--- pages/personal/mypost/mypost.vue
+++ pages/personal/mypost/mypost.vue
@@ -1,6 +1,22 @@
 <template>
-	<view>
-		wode tiezi
+	<view class="container">
+		<view class="list">
+			<view class="item">
+				<view class="title">AMD yes最早起源于ryzen发布后老莱的视频，经过AMD吧和b站
+弹幕发扬光大能不能战未来？我觉得OK</view>
+				<view class="pic-list">
+					<view class="pic"></view>
+				</view>
+				<view>
+					<view class="text-gray text-sm text-left padding">
+						<text class="cuIcon-time margin-lr-xs"></text> 2
+						<text class="cuIcon-attention margin-lr-xs icon"></text> 2
+						<text class="cuIcon-like margin-lr-xs icon"></text> 2
+						<text class="cuIcon-appreciate margin-lr-xs icon"></text> 2
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,7 +24,16 @@
 	export default {
 		data() {
 			return {
-				
+				dataList:[
+					{
+						title:'',
+						pic:[1,2,4],
+						time:'gg',
+						look:22,
+						comment:20,
+						dz:20
+					}
+				]
 			}
 		},
 		methods: {
@@ -17,6 +42,14 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+	.list{
+		.item{
+			width:100%;
+			height:333rpx;
+			margin-top:5px;
+			padding:30rpx;
+			background-color: #fff;
+		}
+	}
 </style>

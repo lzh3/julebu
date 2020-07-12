7824b9496721ed77b839b60df18ad2d810a85e6e
jzsn <jzsn2016@qq.com>
2020-07-12 15:35:22 +0800

    '未登录状态组件'

--- pages/activity/activity.vue
+++ pages/activity/activity.vue
@@ -1,18 +1,22 @@
 <template>
-	<view>
-		活动
+	<view class="activity-wrap">
+		<NotLogin pageName="活动" />
 	</view>
 </template>
 
 <script>
+	import NotLogin from "@/components/NotLogin";
 	export default {
+		components: {
+			NotLogin
+		},
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>

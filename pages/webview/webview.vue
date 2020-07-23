f666b9d7c248a0b40ccf7305b37af7e4061e2ac2
jzsn <jzsn2016@qq.com>
2020-07-24 00:15:03 +0800

    个人页面模块接口数据完成

---
+++ pages/webview/webview.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<web-view :src="url"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url: ''
+			};
+		},
+		onLoad(data) {
+			console.log("TCL: onLoad -> data", data)
+			this.url = data.url
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
